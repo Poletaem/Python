672c02caef80e23fc3ceb1ed7dd2782f85892a1a
Kseniya <125418526+Poletaem@users.noreply.github.com>
2023-06-05 12:15:04 +0500

    Update HW_2.py

--- 5_Les/HomeWork/HW_2.py
+++ 5_Les/HomeWork/HW_2.py
@@ -6,17 +6,3 @@
 #     10
 #     *Вывод:*
 #     1010
-
-A = int(input("Введите число: "))
-list = []
-def convert(n):
-    if (n == 0):
-        return list
-    dig = n % 2
-    list.append(dig)
-    convert(n // 2)
-convert(A)
-list.reverse()
-print("Двоичная форма числа: ")
-for i in list:
-    print(i)
