2967dd97691b0a89ad5ecc4fc891b444a6dbb01e
Ksenia <stopisem@inbox.ru>
2023-06-17 12:41:55 +0300

    9_Les

--- 7_Les/HomeWork/HW_2.py
+++ 7_Les/HomeWork/HW_2.py
@@ -16,12 +16,13 @@
 # 5  10 15 20 25 30
 # 6  12 18 24 30 36 
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
-   print(' '.join([str(i) for i in range(1, num_columns +1)]))    #просто вывели на экран первую строку, соеденив элементы пробелами
-   for i in range(2, num_rows + 1):
-      print(i, end=' \t')
-      for j in range(2, num_columns + 2):
-         print(operation(i, j), end=' \t')
-      print()
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#    print(' '.join([str(i) for i in range(1, num_columns +1)]))    #просто вывели на экран первую строку, соеденив элементы пробелами
+#    for i in range(2, num_rows + 1):
+#       print(i, end=' \t')
+#       for j in range(2, num_columns + 2):
+#          print(operation(i, j), end=' \t')
+#       print()
 
-print_operation_table(lambda x, y: x + y)
+# print_operation_table(lambda x, y: x + y)
+
